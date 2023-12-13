4394a8673d47a8ca9a85fafcbd7470abf909b407
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2023-12-13 14:28:44 +0000

    added url paths

--- project4/network/urls.py
+++ project4/network/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("newPost", views.newPost, name="newPost"),
     path("profile/<int:user_id>", views.profile, name="profile")
+    path("unfollow", views.unfollow, name="unfollow"),
+    path("follow", views.follow, name="follow"),
+
 ]
